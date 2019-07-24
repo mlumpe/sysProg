fc5996a540d412c13e0e363905c13cac20468e87
Jan Feuchter <jan.feuchter@gmail.com>
2019-07-24 09:08:31 +0200

    Haskell A3 fertig

--- hs/Mod_allNodes.hs
+++ hs/Mod_allNodes.hs
@@ -6,11 +6,12 @@ import Types
 import Mod_subtree
 
 allNodes :: Tree -> [Node]
-allNodes tree | isLeaf tree (root tree) = [root tree]
+allNodes tree | size tree == 0 = []
+              | isLeaf tree (root tree) = [root tree]
               | otherwise = [root tree] ++ (helper tree [] 0)
 
 helper :: Tree -> [Node] -> Int -> [Node]
 helper tree res i | i==length childs = res
                   | otherwise = helper tree (res ++ (allNodes (subtree tree (get childs i)))) (i+1)
-				  where
-				   childs = children tree (root tree)
+                  where
+                   childs = children tree (root tree)
--- hs/Mod_isomorphism.hs
+++ hs/Mod_isomorphism.hs
@@ -5,13 +5,25 @@ import Splib
 import Types
 import Mod_treeToString
 import Data.Char
+import Mod_subtree
 
 isomorphism :: Tree -> Tree -> (Map Node Node)
-isomorphism tree1 tree2 = isoString (treeToString tree1) (treeToString tree2) emptyMap
+isomorphism tree1 tree2 | isIsomorph tree1 tree2 = joinMaps (setImage emptyMap (root tree1) (root tree2)) (isoHelper tree1 tree2 0 emptyMap)
+                        | otherwise = emptyMap
+                        
+isoHelper tree1 tree2 i map | i == length (children tree1 (root tree1)) = map
+                            | otherwise = isoHelper tree1 tree2 (i+1) (joinMaps map (isomorphism subtree1 subtree2))
+                                   where
+                                   subtree1 = subtree tree1 (get (children tree1 (root tree1)) i)
+                                   subtree2 = subtree tree2 (get (children tree2 (root tree2)) i)
 
-isoString t1 t2 mapO 
-    | empty t1 && empty t2 = mapO
-	| empty t1 || empty t2 = emptyMap
-	| isDigit (first t1) && isDigit (first t2) = isoString (removeFirst t1) (removeFirst t2) (setImage mapO (first t1) (first t2))
-	| first t1 == first t2 = isoString (removeFirst t1) (removeFirst t2) mapO
-	| otherwise = emptyMap
+    
+isIsomorph tree1 tree2 | isLeaf tree1 (root tree1) && isLeaf tree2 (root tree2) = True
+                       | length (children tree1 (root tree1)) == length (children tree2 (root tree2)) = isIsomorphChilds tree1 tree2 0 True
+                       | otherwise = False
+                       
+isIsomorphChilds tree1 tree2 i iso | i == length (children tree1 (root tree1)) = iso
+                                   | otherwise = sp_and iso (isIsomorph subtree1 subtree2)
+                                   where
+                                   subtree1 = subtree tree1 (get (children tree1 (root tree1)) i)
+                                   subtree2 = subtree tree2 (get (children tree2 (root tree2)) i)
