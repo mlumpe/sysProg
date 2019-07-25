eec1bc3b8bec7ee84b4de8c227bec9790904712d
Jan Feuchter <jan.feuchter@gmail.com>
2019-07-25 13:26:14 +0200

    Option zum Ausschalten der Traces

--- cpp/Mod_stream.cpp
+++ cpp/Mod_stream.cpp
@@ -10,7 +10,7 @@ bool equals(List<Char> window, List<Char> pattern){
 	return true;
 }
 
-List<Int> stream(String str, String pat){
+List<Int> stream_debug(String str, String pat){
 	cout << "string;pattern;window;result" << endl;
 	List<Char> s = s2lc(str);
 	List<Char> pattern = s2lc(pat);
@@ -46,4 +46,38 @@ List<Int> stream(String str, String pat){
 	}
 	return result;
 }
+
+List<Int> stream(String str, String pat){
+	List<Char> s = s2lc(str);
+	List<Char> pattern = s2lc(pat);
+	if(length(s) < length(pattern)){
+		return List<Int>();
+	}
+	List<Char> window;
+	List<Int> result;
+	int i;
+	for(i = 0; i<length(pattern); i++){
+		if(empty(s)){
+			return {};
+		}
+		Char f = first(s);
+		window = append(window, f);
+		s = removeFirst(s);
+	}
+	while(!empty(s)){
+		if(equals(window, pattern)){
+			result = append(result, i-length(pattern));
+		}
+		Char f = first(s);
+		s = removeFirst(s);
+		window = removeFirst(window);
+		window = append(window, f);
+		i++;
+	}
+	if(equals(window, pattern)){
+		result = append(result, i-length(pattern));
+	}
+	return result;
+}
+
 
--- hs/Mod_stream.hs
+++ hs/Mod_stream.hs
@@ -4,17 +4,21 @@ import Debug.Trace
 import Splib
 import Types
 
+trace' :: String -> a -> a
+--trace' str x = (trace str x) 
+trace' str x = x
+
 stream :: String -> String -> [Int]
 stream str pattern | length str < length pattern = []
-                   | otherwise = trace "status;string;pattern;window" (streamHelper str pattern 0 [] [])
+                   | otherwise = trace' "status;string;pattern;window;result" (streamHelper str pattern 0 [] [])
   
 streamHelper :: String -> String -> Int -> String -> [Int] -> [Int]
 streamHelper str pattern k window res 
- | k<length pattern = trace ("Aufbau;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window)) (streamHelper (removeFirst str) pattern (k+1) (append window (first str)) [])
- | empty str && equals window pattern = trace ("Ende+Match;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window)) (res ++ [k-(length pattern)])
- | empty str = trace ("Ende;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window) ) res
- | equals window pattern = trace ("Match;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window)) (streamHelper (removeFirst str) pattern (k+1) (shiftChar window (first str)) (res ++ [k-(length pattern)]))
- | otherwise = trace ("Kein Match;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window)) (streamHelper (removeFirst str) pattern (k+1) (shiftChar window (first str)) res)
+ | k<length pattern = trace' ("Aufbau;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window) ++ ";" ++ (show res)) (streamHelper (removeFirst str) pattern (k+1) (append window (first str)) [])
+ | empty str && equals window pattern = trace' ("Ende+Match;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window) ++ ";" ++ (show res)) (res ++ [k-(length pattern)])
+ | empty str = trace' ("Ende;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window) ++ ";" ++ (show res)) res
+ | equals window pattern = trace' ("Match;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window) ++ ";" ++ (show res)) (streamHelper (removeFirst str) pattern (k+1) (shiftChar window (first str)) (res ++ [k-(length pattern)]))
+ | otherwise = trace' ("Kein Match;" ++ (lc2s str) ++ ";" ++ (lc2s pattern) ++ ";" ++ (lc2s window) ++ ";" ++ (show res)) (streamHelper (removeFirst str) pattern (k+1) (shiftChar window (first str)) res)
 
 equals window pattern 
  | empty window = True
