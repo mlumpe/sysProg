1e842a445050da900ed556090390790f706dbc82
mlumpe <m.lumpe@stud.uni-hannover.de>
2019-07-26 09:36:13 +0200

    compiler probleme?

--- cpp/Mod_lindell.cpp
+++ cpp/Mod_lindell.cpp
@@ -6,11 +6,34 @@
 #include "Mod_nonRecursiveCheck.cpp"
 #include "Mod_sizeLogspace.cpp"
 
-
-
+int kBlockSize(Tree t, Node v, int minimum = 0) {
+    Node kid = firstChild(t,v);
+    int res = sizeLogspace(t, v); //initialwert, da größer als alle k's
+    
+    while (true) {
+        int kidSize = sizeLogspace(t, kid);
+        
+        //cout << "kidSize: " << kidSize << endl;
+        
+        if (kidSize > minimum) {
+            res = min(res, kidSize);
+            //cout << "res : " << res << endl;
+        }
+        
+        if (hasNextSibling(t,kid)) {
+            kid = nextSibling(t, kid);
+        } else {
+            break;
+        }
+    }
+    
+    return res;
+}
 
 int lindell(Tree s, Tree t) {
     int res = 0;
+    int ku = 0;
+    int kv = 0;
     
     if(nonRecursiveCheck(s,t,root(s), root(t))) {
         // nicht isomorph; s[u] < t[v]
@@ -19,25 +42,35 @@ int lindell(Tree s, Tree t) {
         //nicht isomorph; s[u] < t[v]
         return 1;
     } else {
-        Node vs = firstChild(s, root(s));
-        Node vt = firstChild(t, root(t));
-        bool isIsomorph = false;
-        
-        while (hasNextSibling(s, vs)) {
-            while (hasNextSibling(t, vt)) {
-                bool hasIsmorphSubtree = false;
-                if(sizeLogspace(s, vs) == sizeLogspace(t, vt)) {
-                    if(nonRecursiveCheck(s,t,vs,vt)) {
-                        res = -1; // s[u] < t[v]
+        Node u = root(s);
+        Node v = root(t);
+
+        while (true) {
+            ku = kBlockSize(s, u, ku);
+            cout << "ku: " << ku << endl;
+            while (true) {
+                int kv = kBlockSize(t,v, kv);
+                if (kv == ku) {
+                    if(nonRecursiveCheck(s,t,u,v)) {
+                        res = -1;
                     }
-                    
-                    if(nonRecursiveCheck(t,s,vt,vs)) {
-                        res = 1; //t[v] < s[u]
-                    }
+                    cout << u << ";" << v << ";" << ku << ";" << res << ";" << endl;
                 }
-                vt = nextSibling(t, vt);
+                
+                //Abbruchbedingung
+                if (hasNextSibling(t,v)) {
+                    v = nextSibling(t, v);
+                } else {
+                    break;
+                }
             }
-            vs = nextSibling(s, vs);
+            
+            //Abbruchbedingung
+            if (hasNextSibling(s,u)) {
+                u = nextSibling(s, u);
+            } else {
+                break;
+            }
         }
         
         return res;
